5899cdf7d72cf9453f4d15d2b3bcd88093a1cc66
declanflood <declanflood@gmail.com>
2021-06-18 16:05:04 +0100

    Update PowerpointTranslator.py

--- PowerpointTranslator.py
+++ PowerpointTranslator.py
@@ -1,31 +1,81 @@
 from pptx import Presentation
 import six
 import os
-from google.cloud import translate_v2 as translate
+import os.path
+from os import path
+from google.cloud import translate_v3 as translate
 import sys
+from progress.bar import Bar
+import glob
+
+PROJECT_ID = "solar-bolt-244414"
+LOCATION = "us-central1"
+PARENT = f"projects/{PROJECT_ID}/locations/{LOCATION}"
+GLOSSARY_ID = "my_first_glossary"
+
+
+# Translate a file
+def translate_file(filename):
+    if not path.exists(filename):
+        sys.exit("Source file for translation not found")
+
+    prs = Presentation(filename)
+    total_slide_cnt = len(prs.slides)
+    print("Translating " + str(total_slide_cnt) + " slides in " + filename + " from " + str(source_lang_code) + " to " + str(target_lang_code))
+    bar = Bar('Translating: ' + filename, max=total_slide_cnt)
+
+    # cycle through each slide in the file
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            translate_shape(shape)
+        bar.next()
+    bar.finish()
+
+    # save the translated file
+    output_filename = filename.split('.')[0] + "_" + target_lang_code + "2.pptx"
+    try:
+        prs.save(output_filename)
+    except:
+        sys.exit("\n Couldn't save translated file, check if '" + output_filename + "' is already open.")
+    return
 
 
 # Translate a string
-def translate_text(translate_client, target, text):
+def translate_text(text):
 
     if isinstance(text, six.binary_type):
         text = text.decode("utf-8")
 
-    # Passing incorrect target language codes will throw unexpected 403 missing a valid API key error.
-    # Translation API sometimes detects 'es' instead 'pt', passing source_language improves translation quality.
-    result = translate_client.translate(text, target_language=target, source_language='pt', format_='text')
+    # #1 Passing invalid target language codes will throw unexpected 403 missing a valid API key error.
+    # #2 Translation API sometimes detects 'es' instead 'pt', passing source language improves translation quality.
+    response = translate_client.translate_text(
+        request={
+            "parent": PARENT,
+            "contents": [text],
+            "mime_type": "text/plain",  # mime types: text/plain, text/html
+            "source_language_code": source_lang_code,
+            "target_language_code": target_lang_code,
+            "glossary_config": glossary_config,
+        }
+    )
 
-    return result
+    output = ""
+    for translation in response.glossary_translations:
+        output = output + translation.translated_text
+
+    return output
 
 
 # Translate a paragraph
 def translate_paragraph(paragraph):
 
     if (paragraph.text != ''):
-        result = translate_text(translate_client, 'en', paragraph.text)
+        # Translate entire paragraph (typically a sentence), instead of translating a shorter phrase,
+        # for a better quality translation.
+        result = translate_text(paragraph.text)
 
         p = paragraph._p
-        # remove all but the first run (ensures font is not lost)
+        # remove all but the first run (instead of overwriting which could lose the font)
         for idx, run in enumerate(paragraph.runs):
             if idx == 0:
                 continue
@@ -34,13 +84,20 @@ def translate_paragraph(paragraph):
         if (len(paragraph.runs) == 0):
             paragraph.add_run()
 
-        paragraph.runs[0].text = result["translatedText"]
+        # Entire paragraph gets font of the first run, if this was Wingdings then paragraph will be unreadable.
+        # Typically this happens if paragraph is using a Wingdings special character such as a right-arrow,
+        # with the remainder in a normal readable font. Therefore, check for this edge case and force Calibri instead.
+        font = paragraph.runs[0].font
+        if font.name == 'Wingdings':
+            font.name = 'Calibri'
+
+        paragraph.runs[0].text = result
 
     return
 
 
 # Translate all text in a shape
-def convert_shape(shape):
+def translate_shape(shape):
     if (shape.has_text_frame):
         # A shape which contains some text
         for paragraph in shape.text_frame.paragraphs:
@@ -59,23 +116,47 @@ def convert_shape(shape):
     elif(shape.shape_type == 6):
         # Grouped shapes - use recursion
         for shp in shape.shapes:
-            convert_shape(shp)
+            translate_shape(shp)
 
     return
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r".\My_GCP_Translation_API_Key.json"
-translate_client = translate.Client()
 
-filename = sys.argv[1]
 
-prs = Presentation(filename)
+# fetch json key
+my_GCP_key =  glob.glob("My_GCP_Translation_API_Key*.json" )
+if len(my_GCP_key) != 1:
+    sys.exit("There should be one (and only one) json key, exiting....")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = my_GCP_key[0]
 
-total_slide_cnt = len(prs.slides)
+# create translation client and apply glossary
+translate_client = translate.TranslationServiceClient()
+glossary = translate_client.glossary_path(PROJECT_ID, LOCATION, GLOSSARY_ID)
+glossary_config = translate.TranslateTextGlossaryConfig(glossary=glossary)
 
-for slide in prs.slides:
-    print("slide " + str(prs.slides.index(slide) + 1) + "/" + str(total_slide_cnt))
-    for shape in slide.shapes:
-        convert_shape(shape)
+# check arguments passed are valid
+# assume pt to en translation unless overridden by arguments
+source_lang_code = "pt"
+target_lang_code = "en"
+if len(sys.argv) == 4:
+    source_lang_code = sys.argv[2].replace("-","")
+    target_lang_code = sys.argv[3].replace("-","")
 
-output_filename = filename.split('.')[0] + '_en2.pptx'
-prs.save(output_filename)
+# make a list of pptx files in the current directory to translate
+files_to_translate = []
+if sys.argv[1] == "-all":
+    # user wants to translate all pptx files in current folder
+    all_pptx_files = glob.glob('*.pptx')
+    # ignore any that are an output from a previous translation
+    # eg if translating to en then anything ending in "_en2.pptx" is something we previously created
+    ignore_1 = "_" + target_lang_code + ".pptx"
+    ignore_2 = "_" + target_lang_code + "2.pptx"
+    for file in all_pptx_files:
+        if not(ignore_1 in file or ignore_2 in file):
+            files_to_translate.append(file)
+else:
+    # user just wants to translate the named file
+    files_to_translate.append(sys.argv[1])
+
+
+for file in files_to_translate:
+    translate_file(file)
